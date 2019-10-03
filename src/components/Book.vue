cb42e4e05ce81a1f405f9ed88ddde7f4351bf8dc
Mladen Krivacevic <mladen.krivacevic@gmail.com>
2019-10-03 19:48:31 +0200

    Modified route; created BookList.vue and Book.vue components

---
+++ src/components/Book.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>
+    one book.....
+  </section>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/BookList.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="book">
+    <book v-for="(book, index) in books" :book="book" :index="index" :key="book.title"></book>
+  </section>
+</template>
+
+<script>
+import Book from './Book';
+
+export default {
+  components: {
+    book: Book,
+  },
+  data() {
+    return {
+      books: 'some books',
+    };
+  },
+};
+</script>
