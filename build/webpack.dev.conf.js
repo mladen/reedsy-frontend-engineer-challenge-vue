cc3a59431d4b98441bc71849615968fe287d9763
Mladen Krivacevic <mladen.krivacevic@gmail.com>
2020-08-11 23:11:23 +0200

    Modified webpack.dev.conf.js by adding extract-text-webpack-plugin

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -7,6 +7,7 @@ const path = require('path')
 const baseWebpackConfig = require('./webpack.base.conf')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 const portfinder = require('portfinder')
 
@@ -64,7 +65,11 @@ const devWebpackConfig = merge(baseWebpackConfig, {
         to: config.dev.assetsSubDirectory,
         ignore: ['.*']
       }
-    ])
+    ]),
+    // extract css into its own file
+    new ExtractTextPlugin({
+      filename: 'style.css'
+    }),
   ]
 })
 
